950927abf8539fed0c231b040a67aede41c048de
aidankeighron <aidankeighron@gmail.com>
2024-02-11 12:18:30 -0500

    attempt

--- problems1-10.py
+++ problems1-10.py
@@ -25,3 +25,63 @@ import math
 #     num += int(i > 1 and all(i % j != 0 for j in range(2, int(math.sqrt(i)) + 1)))
 # print(i)
 # 8. Largest product in a series
+series = '''73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450'''
+series = series.split('\n')
+
+
+def dfs(point, seen, product, length):
+    if length == 13:
+        return product
+    new_points = []
+    seen.add(point)
+    
+    if point[0] > 0:
+        new_points.append((point[0]-1, point[1]))
+    if point[1] > 0:
+        new_points.append((point[0], point[1]-1))
+    if point[0] < len(series)-1:
+        new_points.append((point[0]+1, point[1]))
+    if point[1] < len(series[0])-1:
+        new_points.append((point[0], point[1]+1))
+    
+    # if point[0] > 0 and point[1] > 0:
+    #     new_points.append((point[0]-1, point[1]-1))
+    # if point[1] > 0 and point[0] < len(series)-1:
+    #     new_points.append((point[0]+1, point[1]-1))
+    # if point[0] < len(series)-1 and point[1] < len(series[0])-1:
+    #     new_points.append((point[0]+1, point[1]+1))
+    # if point[1] < len(series[0])-1 and point[0] > 0:
+    #     new_points.append((point[0]-1, point[1]+1))
+        
+    for new in new_points:
+        if new not in seen:
+            s = dfs(new, seen, product*int(series[new[0]][new[1]]), length+1)
+            return max(product, s or 0)
+
+
+max_p = 0
+for i in range(len(series)):
+    for j in range(len(series[i])):
+        s = dfs((i, j), set(), int(series[i][j]), 1)
+        max_p = max(max_p, s or 0)
+
+print(max_p)
