f54d6d08753f2df1d90d1c9cf83c3f9f7745813f
aidankeighron <aidankeighron@gmail.com>
2024-04-27 16:06:45 -0500

    minified

--- problems11-20.py
+++ problems11-20.py
@@ -180,28 +180,23 @@ from functools import reduce
 # tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 # print(sum(len(single[i]) if i<20 else (len(tens[i//10])+len(single[i%10])) if i<100 else (len(single[i//100])+7)+(0 if j==0 else (3+len(single[j])) if j<20 else (3+len(tens[j//10])+len(single[j%10]))) for i in range(1,1000) if (j := i-(i//100)*100) != -1)+11)
 # 18. Maximum Path Sum 1
-pyramid = """75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23""".split('\n')
-
-def max_sum(i,j):
-    if i==14:
-        return a[i][j]
-    return a[i][j]+max(max_sum(i+1,j),max_sum(i+1,j+1))
-a=[]
-for i in range(15):
-    b=list(map(int,pyramid[i].split()))
-    a.append(b)
-print(max_sum(0,0))
+# pyramid = """75
+# 95 64
+# 17 47 82
+# 18 35 87 10
+# 20 04 82 47 65
+# 19 01 23 75 03 34
+# 88 02 77 73 07 63 67
+# 99 65 04 28 06 16 70 92
+# 41 41 26 56 83 40 80 70 33
+# 41 48 72 33 47 32 37 16 94 29
+# 53 71 44 65 25 43 91 52 97 51 14
+# 70 11 33 28 77 73 17 78 39 68 17 57
+# 91 71 52 38 17 14 91 43 58 50 27 29 48
+# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23""".split('\n')
+# graph = [list(map(int,pyramid[i].split())) for i in range(15)]
+# print((max_sum := lambda i,j: graph[i][j] if i == 14 else graph[i][j]+max(max_sum(i+1,j),max_sum(i+1,j+1)))(0,0))
+# 19. Counting Sundays
+# day, sundays = 0, 0
+# print([[(sundays := sundays + ((day := (day + month + (month == 28 and year % 4 == 0 and (year % 100 == 0 and year % 400 != 0)))) % 7 == 0)) for month in [31,28,31,30,31,30,31,31,30,31,30,31]] for year in range(1901,2000+1)][-1][-1])
