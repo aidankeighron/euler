d8bff944dd1f339eed0a7a91d3133f7ff649c638
aidankeighron <aidankeighron@gmail.com>
2024-05-09 18:12:06 -0500

    day 22

--- problems21-31.py
+++ problems21-31.py
@@ -1,3 +1,5 @@
-import math
+import math, time
 # 21. Amicable Numbers
 # print(sum(i for i in range(10000+1) if (d := lambda n: sum(j+n//j for j in range(2, int(math.sqrt(n))+1) if n % j == 0)+1)(1) and i == d(d(i)) and i != d(i)))
+# 22. Names Scores
+# print(sum(sum(ord(ch) - ord('A') + 1 for ch in name) * (i+1) for i, name in enumerate(sorted([line.split(",") for line in open('./txt/names.txt')][0]))))
