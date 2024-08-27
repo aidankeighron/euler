81c36c4ba4b419da3465a00ceeadc0994a4b6520
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-08-27 15:45:34 -0400

    32

--- problems31-40.py
+++ problems31-40.py
@@ -1,24 +1,38 @@
+import itertools
+
 # 31. Coin Sums
-target = 200
-coins = [1, 2, 5, 10, 20, 50, 100, 200]
+# target = 200
+# coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-ways = [0]*(target+1)
-ways[0] = 1
-for x in coins:
-    for i in range(x, target+1):
-        ways[i] += ways[i-x]
+# ways = [0]*(target+1)
+# ways[0] = 1
+# for x in coins:
+#     for i in range(x, target+1):
+#         ways[i] += ways[i-x]
 
-print(ways[200])
-
-
-mat = [[0]*(target+1)]
-mat[0][0] = 1
-
-for coin in range(1, len(coins)+1):
-    mat.append([0]*(target+1))
-    for x in range(target+1):
-        mat[coin][x] = (mat[coin][x-coins[coin-1]] if x-coins[coin-1] > -1 else 0) + mat[coin-1][x]
+# print(ways[200])
 
 
-print(mat[-1][-1])
-print(mat[-1][-1] == 73682)
+# mat = [[0]*(target+1)]
+# mat[0][0] = 1
+
+# for coin in range(1, len(coins)+1):
+#     mat.append([0]*(target+1))
+#     for x in range(target+1):
+#         mat[coin][x] = (mat[coin][x-coins[coin-1]] if x-coins[coin-1] > -1 else 0) + mat[coin-1][x]
+
+
+# print(mat[-1][-1])
+# print(mat[-1][-1] == 73682)
+# 32. Pandigital Products
+# digits = ["1","2","3","4","5","6","7","8","9"]
+# t = set()
+
+# for i in range(1, 5):
+#     for option in itertools.permutations(digits, i):
+#         for j in range(5-i, 6-i):
+#             for num in itertools.permutations(set(digits) - set(option), j):
+#                 prod = int(''.join(option)) * int(''.join(num))
+#                 if len(option) + len(num) + len(str(prod)) == 9 and digits == sorted(list(option) + list(num) + list(str(prod)[:])):
+#                     t.add(prod)
+# print(sum(list(t)))
